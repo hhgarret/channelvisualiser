6110207e08b8af1d3d507b4d805b2ae0d9836145
hhgarret <129196661+hhgarret@users.noreply.github.com>
2024-04-01 10:27:43 -0400

    Add files via upload

--- channelvisualisermin.py
+++ channelvisualisermin.py
@@ -42,26 +42,15 @@ fftlength = .5 #.5 of a second
 fftbinwidth = 1/fftlength #as fftlength decreases, binwidth increases
 
 fftMode = False
-
-#The toggleFaucet method exists to interact with the xstream process, namely by turning on or off the flow of data from xstream to stdout/stdin.
-#This is achieved by writing either '0' or '1' to /tmp/xstreamControl, a pipe which is read in by xstream
-#When toggled off, the faucet flag is set to False, which turns off readin(). When turned back on, readin() should be also called.
-
-
-#This method helps map the enabled/disabled axes to the plotaxes. This essentially places a given n in the first available space in arr.
-#If given 1 and [0,1,2,4], it would return 3 (can't place at 1, so check at +1. Can't place at 2, so check at +2. 3 is open, so return 3)
-def considerateaddition(n, arr):
-	saven = n
-	for num in arr:
-		if num <= saven:
-			saven += 1
-	return saven
-#Used to get a balanced width/height for any given number of plots.
-#sufficiently close integer factors of n
-def factor_int(n): 
-	a = math.floor(math.sqrt(n))
-	b = math.ceil(n/float(a))
-	return a, b
+curselected = 0
+def changeselected(leftorright):
+	global curselected, maxchannels
+	if leftorright == "left":
+		curselected = (curselected - 1)%maxchannels
+	else:
+		curselected = (curselected + 1)%maxchannels
+	selection.delete("1.0", "1.100")
+	selection.insert(INSERT, "Selected: "+str(curselected))
 #the on_draw method is called whenever the on_draw event is triggered
 def on_draw(event):
 	a = 1
@@ -70,26 +59,6 @@ def on_draw(event):
 	
 #the on_click method is called whenever the on_click event is triggered
 #Here, it checks to see if a plot was clicked, then determined which plot was clicked, and essentially minimizes that plot.
-def on_click(event):
-	global old_fig_size, window, fig, plt, disabledaxes, disabledcount, axes, numchannels, curchannels
-	#event.inaxes.set_axis_off()
-	if numchannels == 2:
-		return
-	if event.inaxes is not None and np.where(axes.flat == event.inaxes)[0][0] < numchannels:
-		event.inaxes.set_axis_off()
-		disabledaxes.append(event.inaxes)
-		actindex = considerateaddition(np.where(axes.flat == event.inaxes)[0][0], disabledindex)
-		#print(actindex)
-		disabledindex.append(actindex)
-		numchannels -= 1
-	
-		old_fig_size = old_fig_size - [1,1]
-		
-		allchannels = {i for i in range(maxchannels)}
-		dischannels = {i for i in disabledindex}
-		curset = allchannels - dischannels
-		curchannels = [elem for elem in curset]
-		init_fig()
 
 
 plots = []
@@ -102,56 +71,37 @@ flushflag = False
 #init_fig initalizes fig and axes for any given enabled channels. Called once at the start, and also whenever a channel is disabled.
 def init_fig():
 	print('init_fig')
-	global plots, totallength, fig, axes, xvals, ylim, numchannels, disabledindex, numchannels, canvas, window, maxchannels, fftMode, old_fig_size, decimationfactor, totallength, appendlength, charts, tempcharts
+	global plots, totallength, fig, axes, xvals, ylim, numchannels, disabledindex, numchannels, canvas, window, maxchannels, fftMode, old_fig_size, decimationfactor, totallength, appendlength, charts, tempcharts, curselected
 	decimationfactor = 10
 	totallength = int(48000 / (2*decimationfactor))
 	appendlength = int(4800 / (decimationfactor))
 	charts = np.zeros((maxchannels, totallength))
 	tempcharts = np.zeros((maxchannels, appendlength))
-	fftMode = False
 	plots = []
 	xvals = range(totallength)
-	width, height = factor_int(numchannels)
+	width, height = (1, 1)
 	plt.close()
 	fig, axes = plt.subplots(nrows=height, ncols=width, squeeze=True, sharex=True,sharey=True)
 	ylim = .05
-	plt.connect('button_press_event', on_click)
 	plt.connect('draw_event', on_draw)
 	skipped = 0
-	if maxchannels > 1:
-		for i in range(maxchannels):
-		    if i in disabledindex:
-		    	skipped += 1
-		    	continue
-		    ms = "{:.1f}".format((2+(maxchannels-numchannels)/2)/10)
-		    pltline, = axes.flat[i-skipped].plot(charts[i], '.k', ms=ms, ls='', alpha=1, animated=True, zorder=10)
-		    plots.append(pltline)
-		    axes.flat[i-skipped].set_xlim(0, totallength)
-		    axes.flat[i-skipped].set_ylim(-1*ylim, ylim)
-		    axes.flat[i-skipped].draw_artist(pltline)
-		    axes.flat[i-skipped].set_yticks([])
-		    axes.flat[i-skipped].set_xticks([])
-		    axes.flat[i-skipped].set_title((i+1), fontsize='small',loc='left')
-		    
-		
-		labels = [str(plot.get_label()) for plot in plots]
-		visibility = [plot.get_visible() for plot in plots]
-		#rax = fig.inset_axes([0.0, 0.0, 0.12,0.2])
-		#check = CheckButtons(rax, labels, visibility)
-		buttons = []
-			
-		#print(ms)
-		plt.tight_layout()
-		plt.connect('button_press_event', on_click)
-		canvas.get_tk_widget().pack_forget()
-		canvas.get_tk_widget().destroy()
-		canvas = FigureCanvasTkAgg(fig, master = window)
-		canvas.draw()
-		canvas.get_tk_widget().pack(fill='both',expand=True,side='top')
-		old_fig_size = old_fig_size - [1,1]
-		diff =  (height*width) - numchannels
-		for i in range(1, diff+1):
-			axes.flat[-1*i].set_axis_off()
+	pltline, = axes.plot(charts[curselected], '.k', ms=".10", ls='', alpha=1, animated=True, zorder=10)
+	plots = pltline
+	axes.set_xlim(0, totallength)
+	axes.set_ylim(-1*ylim, ylim)
+	axes.draw_artist(pltline)
+	axes.set_yticks([])
+	axes.set_xticks([])
+	axes.set_title((curselected+1), fontsize='small',loc='left')
+
+	plt.tight_layout()
+	canvas.get_tk_widget().pack_forget()
+	canvas.get_tk_widget().destroy()
+	canvas = FigureCanvasTkAgg(fig, master = window)
+	canvas.draw()
+	canvas.get_tk_widget().pack(fill='both',expand=True,side='top')
+	old_fig_size = old_fig_size - [1,1]
+	diff =  (height*width) - numchannels
 
 
 
@@ -180,6 +130,19 @@ def resetfig():
 masterFrame = Frame(master = window)
 masterFrame.pack()
 
+
+selectionFrame = Frame(master = masterFrame)
+selectionFrame.pack()
+selectleft = Button(master = selectionFrame, command = lambda:updatewidth("add"), height=2, width=20, text="<--")
+selectleft.pack()
+window.bind('<Right>', lambda event:changeselected("left"))
+selection = Text(master = selectionFrame, height = 1)
+selection.insert(INSERT, "Selected: "+str(curselected))
+selection.pack()
+selectright = Button(master = selectionFrame, command = lambda:updatewidth("remove"), height=2, width=20, text="-->")
+selectright.pack()
+window.bind('<Left>', lambda event:changeselected("right"))
+
 init_fig()
 window.attributes('-zoomed', True)
 
@@ -205,13 +168,6 @@ def readflush():
 		changelength = len(data)
 
 
-#once everything is ready, turn on the faucet and start streaming in data
-try:
-	fc = open("/tmp/xstreamControl", "ab")
-	fc.write(b"\x01")
-	fc.close()
-except:
-	print("exception")
 if binaryFlag is False:
 	tempmaxchannels = len(sys.stdin.readline().split("\t"))
 	if(tempmaxchannels != maxchannels):
@@ -237,34 +193,11 @@ elif binaryFlag is True:
 		init_fig()
 	print(maxchannels)
 
-mb = Menubutton(masterFrame, text="FFT Channels", relief=RAISED)
-#mb.grid()
-mb.menu = Menu(mb)
-checkbuttonvars = []
-prevselected = []
-def printSelectedOptions():
-	global checkbuttonvars, prevselected, fftToggleVar
-	selectedOptions = [count for (count, var) in enumerate(checkbuttonvars) if var.get()]
-	latestOption = np.setdiff1d(selectedOptions, prevselected)
-	print(latestOption)
-	if len(selectedOptions) > 4:
-		checkbuttonvars[latestOption[0]].set(False)
-	else:
-		prevselected = selectedOptions
-	print(prevselected)
-	if(fftToggleVar.get() == 1):
-		init_fft()
-mb['menu'] = mb.menu
-for i in range(maxchannels):
-	var = BooleanVar()
-	checkbuttonvars.append(var)
-	mb.menu.add_checkbutton(variable=var, label=str(i+1), command=lambda:printSelectedOptions())
-mb.pack()
 
 sleeptimer = 0
 latencycount = 0
 async def readin():
-	global flushflag, faucet, decimationcount, decimationfactor, fig, axes, tempcharts, count, old_fig_size, bg, charts, plots, framecount, sleeptimer, latencyText, starttime, latencycount, fftMode, prevselected, fftbinwidth, nyquist
+	global flushflag, faucet, decimationcount, decimationfactor, fig, axes, tempcharts, count, old_fig_size, bg, charts, plots, framecount, sleeptimer, latencyText, starttime, latencycount, fftMode, prevselected, fftbinwidth, nyquist, curselected
 	for line in sys.stdin:
 		if faucet is False:
 			return
@@ -286,42 +219,16 @@ async def readin():
 		        	old_fig_size = fig.get_size_inches()
 			fig.canvas.restore_region(bg)
 			charts = np.concatenate((charts[:,appendlength:],tempcharts), axis=1)
-			if fftMode is False:
-				for i in range(numchannels):
-					plots[i].set_data(xvals, charts[curchannels[i]])
-					#plots[i].set_data(xvals, np.fft.fft(charts[curchannels[i]]))
-					axes.flat[i].draw_artist(plots[i])
-			elif len(prevselected) > 1:
-				for count, i in enumerate(prevselected):
-					y = np.fft.fft(charts[curchannels[i]])
-					y1, y2 = np.split(y, 2)
-					y2 = y2[::-1]
-					y = np.sqrt(np.square(y1) + np.square(y2))
-					x = range(0, nyquist, int(fftbinwidth))
-					plots[count].set_data(x, y)
-					axes.flat[count].draw_artist(plots[count])
-			elif len(prevselected) == 1:
-				y = np.fft.fft(charts[prevselected[0]])
-				y1, y2 = np.split(y, 2)
-				y2 = y2[::-1]
-				y = np.sqrt(np.square(y1) + np.square(y2))
-				x = range(0, nyquist, int(fftbinwidth))
-				plots[0].set_data(x, y)
-				plots[0].set_data(x, y)
-				axes.draw_artist(plots[0])
+			plots.set_data(xvals, charts[curselected])
+			axes.draw_artist(plots)
 			fig.canvas.blit(fig.bbox)
 			fig.canvas.flush_events()
-			framecount += appendlength * decimationfactor / 1000
-			latencyText.delete("1.0", "1.50")
-			latencyval = (48*(time.time()-starttime) - framecount)
-			latencyText.insert(INSERT, "Frame Delta: "+("%.0f"%(latencycount-latencyval))+"k")
-			latencycount = 0.1*latencyval + 0.9*latencycount
 			count = 0
 	print(time.time() - starttime)
 	
 
 async def readinbinary():
-	global flushflag, faucet, decimationcount, decimationfactor, fig, axes, tempcharts, count, old_fig_size, bg, charts, plots, framecount, sleeptimer, latencyText, starttime, latencycount, fftMode, prevselected, fftbinwidth, nyquist, maxchannels
+	global flushflag, faucet, decimationcount, decimationfactor, fig, axes, tempcharts, count, old_fig_size, bg, charts, plots, framecount, sleeptimer, latencyText, starttime, latencycount, fftMode, prevselected, fftbinwidth, nyquist, maxchannels, curselected
 	structstring = '@'+('d'*maxchannels)
 	structsize = maxchannels*8
 	print(structstring)
@@ -351,36 +258,10 @@ async def readinbinary():
 		        	old_fig_size = fig.get_size_inches()
 			fig.canvas.restore_region(bg)
 			charts = np.concatenate((charts[:,appendlength:],tempcharts), axis=1)
-			if fftMode is False:
-				for i in range(numchannels):
-					plots[i].set_data(xvals, charts[curchannels[i]])
-					#plots[i].set_data(xvals, np.fft.fft(charts[curchannels[i]]))
-					axes.flat[i].draw_artist(plots[i])
-			elif len(prevselected) > 1:
-				for count, i in enumerate(prevselected):
-					y = np.fft.fft(charts[curchannels[i]])
-					y1, y2 = np.split(y, 2)
-					y2 = y2[::-1]
-					y = np.sqrt(np.square(y1) + np.square(y2))
-					x = range(0, nyquist, int(fftbinwidth))
-					plots[count].set_data(x, y)
-					axes.flat[count].draw_artist(plots[count])
-			elif len(prevselected) == 1:
-				y = np.fft.fft(charts[prevselected[0]])
-				y1, y2 = np.split(y, 2)
-				y2 = y2[::-1]
-				y = np.sqrt(np.square(y1) + np.square(y2))
-				x = range(0, nyquist, int(fftbinwidth))
-				plots[0].set_data(x, y)
-				plots[0].set_data(x, y)
-				axes.draw_artist(plots[0])
+			plots.set_data(xvals, charts[curselected])
+			axes.draw_artist(plots)
 			fig.canvas.blit(fig.bbox)
 			fig.canvas.flush_events()
-			framecount += appendlength * decimationfactor / 1000
-			latencyText.delete("1.0", "1.50")
-			latencyval = (48*(time.time()-starttime) - framecount)
-			latencyText.insert(INSERT, "Frame Delta: "+("%.0f"%(latencycount-latencyval))+"k")
-			latencycount = 0.1*latencyval + 0.9*latencycount
 			count = 0
 	print(time.time() - starttime)
 if binaryFlag is False:
